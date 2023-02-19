1b25a2ae63840515290728cd8a2c97f8ec4c1d3f
HelenELee <HelenELee3@outlook.com>
2023-02-19 13:29:01 +0800

    category-routes

--- models/index.js
+++ models/index.js
@@ -5,13 +5,48 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'product_tag_link'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'tag_product_link'
+});
 
+/*
+Reader.hasOne(LibraryCard, {
+  foreignKey: 'reader_id',
+  onDelete: 'CASCADE',
+});
+
+// A reader can have many books
+Reader.hasMany(Book, {
+  foreignKey: 'reader_id',
+  onDelete: 'CASCADE',
+});
+*/
 module.exports = {
   Product,
   Category,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,110 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No categories found!' });
+      return;
+    }
+
+    //console.log("Get all categories");
+    res.status(200).json(categoryData);
+    return;
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    console.log("Get one categories");
+    res.status(200).json(categoryData);
+    
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    console.log("Create new category");
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
+    });
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    console.log("Update 1 category");
+
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+      );
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+    
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    console.log("Delete one category");
+    const categoryData = await Category.destroy({
+      where : {
+        id: req.params.id,
+      }
+    })
+    
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
